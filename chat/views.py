bac972d34e4a9e2a4afdb7cd6340c99304a80a93
minrajbhatta.com <bhattaminraj88@gmail.com>
2024-06-13 15:14:40 +0545

    openai api key added

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
+from openai import OpenAI
 from .models import QuestionAnswer
 
 
 def chatGpt(request):
+    # api_key = "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"
+    
     question=''
     if request.method=='POST':
+        client = OpenAI(api_key=api_key)
         question=request.POST.get('question')
-        response=QuestionAnswer.objects.filter(question=question).first()
-        return render(request, 'answer.html', {'chat':response})
+
+        # hamro_response=QuestionAnswer.objects.filter(question__icontains=question).first()
+        # if hamro_response:
+        #     return render(request, 'answer.html', {'chat':hamro_response})
+        # else:
+        response = client.chat.completions.create(
+            model="gpt-4-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "chat with openai"
+                },
+                {
+                    "role": "user",
+                    "content": question
+                }
+            ],
+            temperature=0.7,
+            max_tokens=150
+        )
+        print(response)
+        content=response.choices[0].message.content
+        data=QuestionAnswer.objects.create(question=question, answer=content)
+        return render(request,'answer.html',{'question':question, 'answer':content})
+
     return render(request,'index.html')
 
